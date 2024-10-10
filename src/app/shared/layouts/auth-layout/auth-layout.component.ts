7b0aaca8b7f4854f62fc3f0e105b5d2e41464680
Adrian Calcinas <adrian@google.com>
2024-10-10 13:11:45 -0400

    fix: Separar la logica del login segun su responsabilidad

--- src/app/shared/layouts/auth-layout/auth-layout.component.ts
+++ src/app/shared/layouts/auth-layout/auth-layout.component.ts
@@ -1,38 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+
 import { MatCardModule } from '@angular/material/card';
-import {
-  FormControl,
-  FormGroupDirective,
-  NgForm,
-  Validators,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
-
-import {ErrorStateMatcher} from '@angular/material/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
 
 @Component({
   selector: 'shared-auth-layout',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule,FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  imports: [MatCardModule],
   templateUrl: './auth-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthLayoutComponent {
-
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-
-  matcher = new MyErrorStateMatcher();
-}
-
-
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
+  public authTitle = input.required<string>();
 }
