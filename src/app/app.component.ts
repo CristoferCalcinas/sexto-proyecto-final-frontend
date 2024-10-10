d76ee34be7e5608308ce7783c6369a2ae74b632d
Adrian Calcinas <adrian@google.com>
2024-10-10 12:52:42 -0400

    WIP: Añadir layout con lógica en auth-layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { AuthLayoutComponent } from "./shared/layouts/auth-layout/auth-layout.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [RouterOutlet, MatSlideToggleModule, AuthLayoutComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
