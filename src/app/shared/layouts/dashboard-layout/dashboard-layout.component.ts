190ebc3902fbf94acd822729c6e251358411ac8f
Adrian Calcinas <adrian@google.com>
2024-10-10 11:33:05 -0400

    feat: Añadir componente de diseño de dashboard

---
+++ src/app/shared/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-dashboard-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-layout.component.html',
+})
+export class DashboardLayoutComponent {
+
+}
