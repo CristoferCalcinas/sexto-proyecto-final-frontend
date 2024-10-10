43137a75613db4e2f992753d42cf93c7cab05e7d
Adrian Calcinas <adrian@google.com>
2024-10-10 14:00:31 -0400

    feat: Añadir diseño de barra lateral al componente de layout del dashboard

--- src/app/shared/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/shared/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,11 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'shared-dashboard-layout',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    RouterModule,
+  ],
   templateUrl: './dashboard-layout.component.html',
 })
 export class DashboardLayoutComponent {
-
+  public sidebarItems = [
+    {
+      label: 'Listado',
+      icon: 'label',
+      url: './list',
+    },
+    {
+      label: 'Añadir',
+      icon: 'add',
+      url: './new-hero',
+    },
+    {
+      label: 'Buscar',
+      icon: 'search',
+      url: './search',
+    },
+  ];
 }
