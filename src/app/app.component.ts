7bfcc156a553f4ff50bf7d24dd25b5e802e5bd33
Adrian Calcinas <adrian@google.com>
2024-10-10 13:35:32 -0400

    feat: Añadir componente de registro con formulario y validaciones nulas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AuthLayoutComponent } from "./shared/layouts/auth-layout/auth-layout.component";
 import { LoginComponent } from "./views/auth/login/login.component";
+import { RegisterComponent } from "./views/auth/register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule, AuthLayoutComponent, LoginComponent],
+  imports: [RouterOutlet, MatSlideToggleModule, AuthLayoutComponent, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/views/auth/register/register.component.ts
+++ src/app/views/auth/register/register.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { AuthLayoutComponent } from '../../../shared/layouts/auth-layout/auth-layout.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormGroupDirective, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'view-register',
   standalone: true,
-  imports: [],
+  imports: [
+    AuthLayoutComponent,
+    MatFormFieldModule,
+    FormsModule,
+    AuthLayoutComponent,
+    MatButtonModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './register.component.html',
-  styles: ``
+  styles: ``,
 })
 export class RegisterComponent {
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
+  matcher = new MyErrorStateMatcher();
+}
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
+  }
 }
