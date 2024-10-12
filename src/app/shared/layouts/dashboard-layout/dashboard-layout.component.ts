eb73920f763d48358d81ad34cce00247fc8dcdb5
Adrian Calcinas <adrian@google.com>
2024-10-12 18:37:35 -0400

    fix: Añadir módulo MatButton a la estructura del diseño del dashboard

--- src/app/shared/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/shared/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -11,6 +12,7 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [
     CommonModule,
+    MatButtonModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
