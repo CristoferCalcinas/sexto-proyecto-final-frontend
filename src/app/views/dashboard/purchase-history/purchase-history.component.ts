f8ab488503e687d5c6435daf324153d8b0dd7e81
Adrian Calcinas <adrian@google.com>
2024-10-12 14:51:46 -0400

    feat: Añadir componente de historial de compras con tabla y cálculo de costos

--- src/app/views/dashboard/purchase-history/purchase-history.component.ts
+++ src/app/views/dashboard/purchase-history/purchase-history.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { TitleComponentComponent } from '@shared/components/title-component/title-component.component';
+
+export interface Transaction {
+  item: string;
+  cost: number;
+}
 
 @Component({
   selector: 'app-purchase-history',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, CurrencyPipe, TitleComponentComponent],
   templateUrl: './purchase-history.component.html',
-  styles: ``
+  styles: ``,
 })
 export default class PurchaseHistoryComponent {
+  displayedColumns = ['item', 'cost'];
+  transactions: Transaction[] = [
+    { item: 'Beach ball', cost: 4 },
+    { item: 'Towel', cost: 5 },
+    { item: 'Frisbee', cost: 2 },
+    { item: 'Sunscreen', cost: 4 },
+    { item: 'Cooler', cost: 25 },
+    { item: 'Swim suit', cost: 15 },
+  ];
 
+  getTotalCost() {
+    return this.transactions
+      .map((t) => t.cost)
+      .reduce((acc, value) => acc + value, 0);
+  }
 }
