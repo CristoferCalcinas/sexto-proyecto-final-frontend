725007f4504fcfbffcb8838f33f766ba9b3f4b9b
Adrian Calcinas <adrian@google.com>
2024-10-29 19:45:54 -0400

    fix: Corregir la URL del servicio de productos para incluir el endpoint correcto

--- src/app/services/products-list.service.ts
+++ src/app/services/products-list.service.ts
@@ -3,9 +3,9 @@ import { inject, Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
-  private readonly urlAllProducts = 'http://localhost:5210/api/Productos';
+  private readonly urlAllProducts = 'http://localhost:5210/api';
   private http = inject(HttpClient);
   getAllProducts() {
-    return this.http.get<any[]>(this.urlAllProducts);
+    return this.http.get<any[]>(`${this.urlAllProducts}/Productos`);
   }
 }
