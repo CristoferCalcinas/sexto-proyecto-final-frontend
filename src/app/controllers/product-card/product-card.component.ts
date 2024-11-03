d139cbba4ae0ca2245476a681e8882f021baa973
Adrian Calcinas <adrian@google.com>
2024-11-02 22:03:59 -0400

    feat: Añadir enlace al detalle del producto en el componente de tarjeta

--- src/app/controllers/product-card/product-card.component.ts
+++ src/app/controllers/product-card/product-card.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'components-product-card',
   standalone: true,
-  imports: [MatCardModule, MatChipsModule, MatProgressBarModule],
+  imports: [MatCardModule, MatChipsModule, MatProgressBarModule, RouterLink],
   templateUrl: './product-card.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
