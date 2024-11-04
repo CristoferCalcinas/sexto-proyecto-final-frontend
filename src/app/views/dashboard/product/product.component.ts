b498eb499ce4c515d660a365a07ce8e8d7630419
Adrian Calcinas <adrian@google.com>
2024-11-04 10:14:16 -0400

    feat: Añadir carga de producto con retraso y método para agregar producto al carrito en el componente de producto

--- src/app/views/dashboard/product/product.component.ts
+++ src/app/views/dashboard/product/product.component.ts
@@ -1,29 +1,42 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { delay, switchMap } from 'rxjs';
 import { ProductsService } from '../../../services/products-list.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styles: ``,
 })
 export default class ProductComponent implements OnInit {
   public product: any = null;
+  public loading = true;
   private activatedRoute = inject(ActivatedRoute);
   private productsService = inject(ProductsService);
   private router = inject(Router);
 
   ngOnInit(): void {
     this.activatedRoute.params
-      .pipe(switchMap(({ id }) => this.productsService.getProductById(id)))
+      .pipe(
+        delay(1500),
+        switchMap(({ id }) => this.productsService.getProductById(id))
+      )
       .subscribe((product) => {
         if (!product) return this.router.navigate(['/dashboard/products-list']);
 
         this.product = product;
+        this.loading = false;
+        console.log(this.product);
         return;
       });
+  }
+
+  addProductToCart() {
+    if (!this.product) return;
+    console.log('Product added to cart');
+    console.log(this.product);
   }
 }
