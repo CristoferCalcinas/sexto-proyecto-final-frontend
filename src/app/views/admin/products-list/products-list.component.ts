92fe68ef90452ad84812394d01bccb1fae5973ea
Adrian Calcinas <adrian@google.com>
2024-11-10 19:10:04 -0400

    feat: Implementar carga asíncrona de productos en la lista de productos

--- src/app/views/admin/products-list/products-list.component.ts
+++ src/app/views/admin/products-list/products-list.component.ts
@@ -1,11 +1,16 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ProductCardComponent } from "../../../controllers/product-card/product-card.component";
+import { ProductsService } from '../../../services/products-list.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-products-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductCardComponent, AsyncPipe],
   templateUrl: './products-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ProductsListComponent {}
+export default class ProductsListComponent {
+  private productsService = inject(ProductsService);
+  public products: Observable<any[]> = this.productsService.getAllProducts();}
