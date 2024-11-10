e50ed80412205b3b782757aaf06319999c4801be
Adrian Calcinas <adrian@google.com>
2024-11-10 17:04:04 -0400

    feat: Simplificar el componente principal eliminando módulos no utilizados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { AuthLayoutComponent } from "./shared/layouts/auth-layout/auth-layout.component";
-import { DashboardLayoutComponent } from "./shared/layouts/dashboard-layout/dashboard-layout.component";
-import DashboardComponent from "./views/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule, AuthLayoutComponent, DashboardLayoutComponent, DashboardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
