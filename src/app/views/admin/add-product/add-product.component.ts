97e7803387e1977ca850713efe6664662ba5761b
Adrian Calcinas <adrian@google.com>
2024-11-23 13:40:57 -0400

    feat: Implementar formulario reactivo para añadir productos con validaciones

--- src/app/views/admin/add-product/add-product.component.ts
+++ src/app/views/admin/add-product/add-product.component.ts
@@ -1,11 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-product.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AddProductComponent {}
+export default class AddProductComponent {
+  private fb = inject(FormBuilder);
+  public myForm = this.fb.group({
+    productTitle: ['', Validators.required],
+    productDescription: ['', Validators.required],
+    productPrice: ['', Validators.required],
+    productQuantity: ['', Validators.required],
+    productCategory: ['', Validators.required],
+    productSupplier: ['', Validators.required],
+  });
+}
