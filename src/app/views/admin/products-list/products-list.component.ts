0529c9faeec7e1a1e8659c8308a462bee0b20145
Adrian Calcinas <adrian@google.com>
2024-11-23 23:51:24 -0400

    feat: Añadir botones de editar y borrar en la lista de productos

--- src/app/views/admin/products-list/products-list.component.ts
+++ src/app/views/admin/products-list/products-list.component.ts
@@ -3,6 +3,8 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ProductCardComponent } from '../../../controllers/product-card/product-card.component';
 import { ProductsService } from '../../../services/products-list.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-products-list',
@@ -13,5 +15,14 @@ import { Observable } from 'rxjs';
 })
 export default class ProductsListComponent {
   private productsService = inject(ProductsService);
+  private router = inject(Router);
   public products: Observable<any[]> = this.productsService.getAllProducts();
+
+  editProduct(product: any) {
+    this.router.navigate(['/admin/edit-product', product.id]);
+  }
+
+  deleteProduct(product: any) {
+    console.log(product);
+  }
 }
