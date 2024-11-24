984e6b77b2f524e914be4bbd4e65c32338b831fc
Adrian Calcinas <adrian@google.com>
2024-11-24 00:49:16 -0400

    feat: Implementar formulario reactivo para editar productos y manejar la actualización

--- src/app/views/admin/edit-product/edit-product.component.ts
+++ src/app/views/admin/edit-product/edit-product.component.ts
@@ -1,10 +1,47 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { ProductsService } from '../../../services/products-list.service';
 
 @Component({
   selector: 'app-edit-product',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './edit-product.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class EditProductComponent { }
+export default class EditProductComponent implements OnInit {
+  private fb = inject(FormBuilder);
+  private productService = inject(ProductsService);
+  private activatedRoute = inject(ActivatedRoute);
+
+  public myForm = this.fb.group({
+    nombreProducto: ['', Validators.required],
+    descripcion: ['', Validators.required],
+    precio: ['', Validators.required],
+    cantidadStock: ['', Validators.required],
+    categoriaId: [''],
+    proveedorId: [''],
+  });
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.productService.getProductById(id)))
+      .subscribe((product) => {
+        if (!product) return;
+        this.myForm.patchValue(product);
+      });
+  }
+
+  updateProduct() {
+    if (this.myForm.invalid) return;
+
+    console.log(this.myForm.value);
+  }
+}
