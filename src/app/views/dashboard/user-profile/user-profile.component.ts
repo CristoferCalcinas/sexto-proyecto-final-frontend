8df4bc1ca971bb2442b77455d994cfc4d7879a02
Adrian Calcinas <adrian@google.com>
2024-11-26 17:34:27 -0400

    feat: Redirigir a la página de inicio de sesión si no se encuentra el ID de usuario en localStorage

--- src/app/views/dashboard/user-profile/user-profile.component.ts
+++ src/app/views/dashboard/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { TitleComponent } from '../../../shared/components/title-component/title-component.component';
 import { UserService } from '../../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,12 +11,20 @@ import { UserService } from '../../../services/user.service';
   styles: ``,
 })
 export default class UserProfileComponent implements OnInit {
+  private router = inject(Router);
   private userProfileService = inject(UserService);
 
   public userProfile: any;
 
   ngOnInit(): void {
-    this.userProfileService.getUserProfileById(1).subscribe((data) => {
+    const userId = localStorage.getItem('userId');
+
+    if (!userId) {
+      this.router.navigate(['/auth/login']);
+      return;
+    }
+
+    this.userProfileService.getUserProfileById(+userId!).subscribe((data) => {
       this.userProfile = data;
     });
   }
