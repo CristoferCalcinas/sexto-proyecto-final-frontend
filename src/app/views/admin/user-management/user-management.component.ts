380e406979a51de1d10bedb48f7a00d02481b89e
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2024-12-09 22:17:05 -0400

    feat: implementar categorización de usuarios en el componente de gestión de usuarios

--- src/app/views/admin/user-management/user-management.component.ts
+++ src/app/views/admin/user-management/user-management.component.ts
@@ -1,7 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { UserListComponent } from './ui/user-list/user-list.component';
 import { EmployeesListComponent } from './ui/employees-list/employees-list.component';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-user-management',
@@ -10,7 +16,41 @@ import { EmployeesListComponent } from './ui/employees-list/employees-list.compo
   templateUrl: './user-management.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class UserManagementComponent {
+export default class UserManagementComponent implements OnInit {
+  private userService = inject(UserService);
   public visibleUserList = true;
   public visibleEmployeeList = true;
+
+  public suppliers: any[] = [];
+  public admins: any[] = [];
+  public customers: any[] = [];
+
+  ngOnInit(): void {
+    // Tener todos los usuarios en un solo arreglo
+    const users = this.userService.getAllUsers().subscribe((users) => {
+
+      const categorizedUsers = {
+        suppliers: [] as any[],
+        admins: [] as any[],
+        customers: [] as any[],
+      };
+
+      // Filtrar usuarios por rol
+      users.forEach((user: any) => {
+        if (user.rol.nombreRol === 'Proveedor') {
+          categorizedUsers.suppliers.push(user);
+        } else if (user.rol.nombreRol === 'Administrador') {
+          categorizedUsers.admins.push(user);
+        } else {
+          categorizedUsers.customers.push(user);
+        }
+      });
+
+      // Asignar a las propiedades de la clase
+      this.suppliers = categorizedUsers.suppliers;
+      this.admins = categorizedUsers.admins;
+      this.customers = categorizedUsers.customers;
+
+    });
+  }
 }
