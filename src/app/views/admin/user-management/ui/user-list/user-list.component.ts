760bba5184fa7dbb63f0c6c87488a8cfa9199999
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2024-12-09 22:01:22 -0400

    feat: actualizar componente de lista de usuarios para usar nombre de usuario y refactorizar la obtención de clientes

--- src/app/views/admin/user-management/ui/user-list/user-list.component.ts
+++ src/app/views/admin/user-management/ui/user-list/user-list.component.ts
@@ -1,19 +1,22 @@
 import { AsyncPipe, CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
 import { UserService } from '../../../../../services/user.service';
 
 @Component({
   selector: 'ui-user-list',
   standalone: true,
-  imports: [CommonModule, AsyncPipe],
+  imports: [CommonModule],
   templateUrl: './user-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserListComponent {
+  public customers: any = input.required();
+
   private userProfileService = inject(UserService);
   public users = this.userProfileService.getAllUsers();
 
   changeUserRole(id: any) {
     console.log(id);
+    console.log(this.customers());
   }
 }
