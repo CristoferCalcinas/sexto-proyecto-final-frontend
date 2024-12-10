86126ecd45cf0c2361cb060ddfeae264ecb6e965
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2024-12-10 15:23:36 -0400

    feat: implementar gestión reactiva de visibilidad de listas de usuarios y optimizar la carga de usuarios

--- src/app/views/admin/user-management/user-management.component.ts
+++ src/app/views/admin/user-management/user-management.component.ts
@@ -4,11 +4,13 @@ import {
   Component,
   inject,
   OnInit,
+  signal,
 } from '@angular/core';
 import { UserListComponent } from './ui/user-list/user-list.component';
 import { EmployeesListComponent } from './ui/employees-list/employees-list.component';
 import { UserService } from '../../../services/user.service';
 import { User } from '@models/user.interface';
+import { catchError, tap } from 'rxjs';
 
 @Component({
   selector: 'app-user-management',
@@ -19,19 +21,33 @@ import { User } from '@models/user.interface';
 })
 export default class UserManagementComponent implements OnInit {
   private userService = inject(UserService);
-  public isVisibleUserList = true;
-  public isVisibleAdminList = true;
-  public isVisibleSupplierList = true;
 
-  public allUsers: User[] = [];
+  // Usar signals para una gestión más reactiva del estado
+  usersSignal = signal<User[]>([]);
+
+  isVisibleUserList = signal(false);
+  isVisibleAdminList = signal(false);
+  isVisibleSupplierList = signal(false);
 
   getUsersByRole(role: string): User[] {
-    return this.allUsers.filter((user) => user.rol.nombreRol === role);
+    return this.usersSignal().filter((user) => user.rol.nombreRol === role);
   }
 
   ngOnInit(): void {
-    this.userService.getAllUsers().subscribe((users) => {
-      this.allUsers = users;
-    });
+    this.userService.getAllUsers().pipe(
+      tap((users) => {
+        // Actualizar el signal con los usuarios cargados
+        this.usersSignal.set(users);
+
+        // Activar automáticamente las listas si hay usuarios en cada categoría
+        this.isVisibleUserList.set(this.getUsersByRole('Cliente').length > 0);
+        this.isVisibleAdminList.set(this.getUsersByRole('Administrador').length > 0);
+        this.isVisibleSupplierList.set(this.getUsersByRole('Proveedor').length > 0);
+      }),
+      catchError((err) => {
+        console.error('Error al cargar usuarios', err);
+        return [];
+      })
+    ).subscribe();
   }
 }
