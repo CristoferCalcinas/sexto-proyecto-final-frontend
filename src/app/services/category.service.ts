67dbe6fd93823dce68cd56807b228954aebc27d3
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2024-12-11 16:10:06 -0400

    feat: actualizar CategoryService para utilizar tipos específicos en las respuestas de la API

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,27 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { environments } from '@env/environments';
 import { catchError, of } from 'rxjs';
+
+import { environments } from '@env/environments';
+import { Category } from '@models/category.interface';
 
 @Injectable({ providedIn: 'root' })
 export class CategoryService {
-  constructor() {}
   private API_URL = `${environments.baseUrl}/Categorium`;
   private http = inject(HttpClient);
 
   getCategories() {
     return this.http
-      .get<any[]>(`${this.API_URL}`)
+      .get<Category[]>(`${this.API_URL}`)
       .pipe(catchError(() => of([])));
   }
 
   addCategory(category: any) {
-    return this.http.post<any>(`${this.API_URL}`, category);
-    // .pipe(catchError(() => of([])));
+    return this.http.post<Category>(`${this.API_URL}`, category);
   }
 
   deleteCategories(ids: number[]) {
-    return this.http.delete<any>(`${this.API_URL}/batch`, {
+    return this.http.delete<Category>(`${this.API_URL}/batch`, {
       body: ids,
     });
   }
