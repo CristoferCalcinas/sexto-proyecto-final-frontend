7fbb1864333bba94928e485f225be1d88266c9cd
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2024-12-11 16:06:56 -0400

    feat: actualizar CategoryService para utilizar la URL base del entorno y simplificar las rutas de la API

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,26 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { environments } from '@env/environments';
 import { catchError, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CategoryService {
   constructor() {}
-  private API_URL = 'http://localhost:5210/api';
+  private API_URL = `${environments.baseUrl}/Categorium`;
   private http = inject(HttpClient);
 
   getCategories() {
     return this.http
-      .get<any[]>(`${this.API_URL}/Categorium`)
+      .get<any[]>(`${this.API_URL}`)
       .pipe(catchError(() => of([])));
   }
 
   addCategory(category: any) {
-    return this.http.post<any>(`${this.API_URL}/Categorium`, category);
+    return this.http.post<any>(`${this.API_URL}`, category);
     // .pipe(catchError(() => of([])));
   }
 
   deleteCategories(ids: number[]) {
-    return this.http.delete<any>(`${this.API_URL}/Categorium/batch`, {
+    return this.http.delete<any>(`${this.API_URL}/batch`, {
       body: ids,
     });
   }
