1ad3920cdb2c0cb54c3294c364827745f3db87d7
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2024-12-13 14:10:40 -0400

    feat: agregar funcionalidad para agregar productos al carrito en el componente FavoriteCard

--- src/app/shared/components/favorite-card-component/favorite-card-component.component.ts
+++ src/app/shared/components/favorite-card-component/favorite-card-component.component.ts
@@ -1,12 +1,18 @@
 import { Component, inject, input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { catchError, of, switchMap } from 'rxjs';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
-import { Product } from '@models/product.interface';
 
+import { CarritoService } from '@services/carrito.service';
+import { DetalleCarritoService } from '@services/detalle-carrito.service';
 import { FavoriteService } from '@services/favorite.service';
 import { ProductsService } from '@services/products.service';
-import { CommonModule } from '@angular/common';
+
+import type { Product } from '@models/product.interface';
 
 @Component({
   selector: 'shared-favorite-card-component',
@@ -19,9 +25,46 @@ export class FavoriteCardComponent implements OnInit {
   public itemId = input.required<number>();
   public favoriteService = inject(FavoriteService);
   private productService = inject(ProductsService);
+  private carritoService = inject(CarritoService);
+  private detalleCarritoService = inject(DetalleCarritoService);
+  private router = inject(Router);
   public product: Product | undefined;
 
   deleteFavorite() {
+    this.favoriteService.removeFromWishList(this.itemId());
+  }
+
+  addProductToCart(): void {
+    const userId = localStorage.getItem('userId');
+
+    if (!userId) {
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    if (!this.product?.id || !this.product?.precio) {
+      console.error('No se puede agregar un producto inexistente al carrito');
+      return;
+    }
+
+    this.carritoService
+      .getCarritoByUserId(+userId)
+      .pipe(
+        switchMap((carrito) =>
+          this.detalleCarritoService.createDetalleCarrito(
+            carrito.id,
+            this.product!.id,
+            this.product!.precio
+          )
+        ),
+        catchError((error) => {
+          console.error('Error al agregar producto al carrito', error);
+          return of(null);
+        })
+      )
+      .subscribe((resp) => {
+        console.log(resp);
+      });
     this.favoriteService.removeFromWishList(this.itemId());
   }
 
