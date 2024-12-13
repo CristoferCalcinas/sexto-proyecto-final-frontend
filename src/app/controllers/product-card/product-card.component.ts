654a1e6b4f299ac84fd56814a2b544f71d512aee
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2024-12-13 11:00:21 -0400

    feat: agregar funcionalidad de favorito en el componente ProductCard

--- src/app/controllers/product-card/product-card.component.ts
+++ src/app/controllers/product-card/product-card.component.ts
@@ -1,17 +1,45 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+} from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
-import { RouterLink } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+
+import { FavoriteService } from '@services/favorite.service';
+import { Product } from '@models/product.interface';
 
 @Component({
   selector: 'components-product-card',
   standalone: true,
-  imports: [MatCardModule, MatChipsModule, MatProgressBarModule, RouterLink],
+  imports: [
+    MatCardModule,
+    MatChipsModule,
+    MatIcon,
+    RouterLink,
+  ],
   templateUrl: './product-card.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductCardComponent {
-  public product: any = input.required();
+  public product = input.required<Product>();
+  public favoriteService = inject(FavoriteService);
+
+  toggleFavorite(event: Event) {
+    // Detener la propagación para que no se active el routerLink
+    event.stopPropagation();
+
+    const productId = this.product().id;
+
+    if (this.favoriteService.isProductInWishList(productId)) {
+      this.favoriteService.removeFromWishList(productId);
+    } else {
+      this.favoriteService.addToWishList(productId);
+    }
+  }
 }
