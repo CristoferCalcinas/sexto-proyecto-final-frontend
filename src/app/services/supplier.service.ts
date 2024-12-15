e7288e1ab47d2af8bfe7a0b5566a27a333277f32
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2024-12-15 12:01:31 -0400

    refactor: actualizar la URL de la API del proveedor para utilizar la configuración del entorno

--- src/app/services/supplier.service.ts
+++ src/app/services/supplier.service.ts
@@ -2,23 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { catchError, of } from 'rxjs';
 
+import { environments } from '@env/environments';
+
 @Injectable({ providedIn: 'root' })
 export class SupplierService {
-  private API_URL = 'http://localhost:5210/api';
+  private API_URL = `${environments.baseUrl}/Proveedor`;
   private http = inject(HttpClient);
 
   getSuppliers() {
     return this.http
-      .get<any[]>(`${this.API_URL}/Proveedor`)
+      .get<any[]>(`${this.API_URL}`)
       .pipe(catchError(() => of([])));
   }
 
   addSupplier(supplier: any) {
-    return this.http.post(`${this.API_URL}/Proveedor`, supplier);
+    return this.http.post(`${this.API_URL}`, supplier);
   }
 
   deleteSuppliers(ids: number[]) {
-    return this.http.delete(`${this.API_URL}/Proveedor/batch`, {
+    return this.http.delete(`${this.API_URL}/batch`, {
       body: ids,
     });
   }
