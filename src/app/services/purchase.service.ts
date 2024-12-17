a7f5590dd64455cc0cf5d5d4192702b54e5c4f40
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2024-12-17 10:20:11 -0400

    fix: agregar un punto de escape ante el error en el cual no se encuentra alguna compra

--- src/app/services/purchase.service.ts
+++ src/app/services/purchase.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 
 import { environments } from '@env/environments';
 import type { Purchase } from '@models/purchase.interface';
@@ -16,6 +16,8 @@ export class PurchaseService {
   // }
 
   getPurchaseHistoryByUserId(userId: string): Observable<Purchase> {
-    return this.http.get<Purchase>(`${this.API_URL}/purchases/user?userId=${userId}`);
+    return this.http
+      .get<Purchase>(`${this.API_URL}/purchases/user?userId=${userId}`)
+      .pipe(catchError(() => of({} as Purchase)));
   }
 }
